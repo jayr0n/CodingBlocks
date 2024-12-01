4cc8166aab3a1657801fccfd61b4a69bc545145f
Jayron <jayron@Hotmail.com.br>
2024-12-01 20:00:05 -0300

    adding video example

--- Opencv_KnnProject/cv_video.py
+++ Opencv_KnnProject/cv_video.py
@@ -1,19 +1,23 @@
 import cv2
 
 cap = cv2.VideoCapture(0)
-
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
 while True:
 	ret, frame = cap.read()
-	gray_grame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	#gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+	gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
 	if ret == False:
 		continue
 
+	faces = face_cascade.detectMultiScale(gray_frame, 1.3, 5)
+
+	for (x,y,w,h) in faces:
+		cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 2)
 	cv2.imshow("Video Frame", frame)
-	cv2.imshow("Gray Frame", gray_grame)
 
 	# Wait for user input -q, the you will stop the loop
 	key_pressed = cv2.waitKey(1) & 0xFF
-
 	if key_pressed == ord('q'):
 		break
 
